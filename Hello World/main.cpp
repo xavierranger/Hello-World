614289087515592ebf819de92845a78f86759f95
Xiaotong Ding <apple@dyn-160-39-197-80.dyn.columbia.edu>
2014-09-08 17:06:25 -0400

    Initial Commit

---
+++ Hello World/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Hello World
+//
+//  Created by Xiaotong Ding on 9/8/14.
+//  Copyright (c) 2014 Xiaotong Ding. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
